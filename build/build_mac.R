cfba5a27af11a0705bf348f34a24a2a0e19dbe63
vnijs <radiant@rady.ucsd.edu>
2016-06-29 15:46:09 -0700

    build fix for inst/rstudio

--- build/build_mac.R
+++ build/build_mac.R
@@ -1,4 +1,8 @@
 ## build for mac
-devtools::install("~/gh/radiant.design")
-devtools::build("~/gh/radiant.design")
-devtools::build("~/gh/radiant.design", binary = TRUE)
+app <- "radiant.design"
+path <- "~/gh"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
--- build/build_win.R
+++ build/build_win.R
@@ -1,3 +1,9 @@
 ## build for windows
-devtools::install("~/gh/radiant.design")
-devtools::build("~/gh/radiant.design", binary = TRUE)
+app <- "radiant.design"
+path <- "../"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
+
