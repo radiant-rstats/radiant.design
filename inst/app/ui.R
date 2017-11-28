6529cb1ee707521f0b3fe84e278d0620c4e0ee60
vnijs <radiant@rady.ucsd.edu>
2017-11-28 13:05:10 -0800

    update

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,5 +1,5 @@
 ## ui for design menu in radiant
-do.call(navbarPage,
+navbar_proj(do.call(navbarPage,
   c("Radiant", getOption("radiant.nav_ui"), getOption("radiant.design_ui"),
     getOption("radiant.shared_ui"), help_menu("help_design_ui"))
-)
+))
