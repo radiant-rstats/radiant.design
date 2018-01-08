ae1a3a4ad5adf9fd01b6b447aaa8b9d5f295ee4e
vnijs <radiant@rady.ucsd.edu>
2018-01-08 12:25:11 -0800

    cleanup

--- inst/app/server.R
+++ inst/app/server.R
@@ -3,6 +3,19 @@ shinyServer(function(input, output, session) {
   ## source shared functions
   source(file.path(getOption("radiant.path.data"), "app/init.R"), encoding = getOption("radiant.encoding"), local = TRUE)
   source(file.path(getOption("radiant.path.data"), "app/radiant.R"), encoding = getOption("radiant.encoding"), local = TRUE)
+
+  ## source data & app tools from radiant.data
+  for (file in list.files(
+    c(
+      file.path(getOption("radiant.path.data"), "app/tools/app"),
+      file.path(getOption("radiant.path.data"), "app/tools/data")
+    ),
+    pattern = "\\.(r|R)$", 
+    full.names = TRUE)) {
+    source(file, encoding = getOption("radiant.encoding"), local = TRUE)
+  }
+
+  ## setting up help
   source("help.R", encoding = getOption("radiant.encoding"), local = TRUE)
 
   ## help ui
@@ -24,16 +37,6 @@ shinyServer(function(input, output, session) {
 
   ## packages to use for example data
   options(radiant.example.data = c("radiant.data", "radiant.design"))
-
-  ## source data & app tools from radiant.data
-  for (file in list.files(
-    c(
-      file.path(getOption("radiant.path.data"), "app/tools/app"),
-      file.path(getOption("radiant.path.data"), "app/tools/data")
-    ),
-    pattern = "\\.(r|R)$", full.names = TRUE
-  ))
-    source(file, encoding = getOption("radiant.encoding"), local = TRUE)
 
   ## 'sourcing' radiant's package functions in the server.R environment
   if (!"package:radiant.design" %in% search() && getOption("radiant.path.design") == "..") {
