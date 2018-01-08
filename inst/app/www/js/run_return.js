7995aa3706a162fb23f3d986e5852bafa2dcfca8
vnijs <radiant@rady.ucsd.edu>
2018-01-08 12:23:49 -0800

    btn-primary

--- inst/app/www/js/run_return.js
+++ inst/app/www/js/run_return.js
@@ -1,19 +1,18 @@
 // based on http://stackoverflow.com/a/32340906/1974918
 // and http://stackoverflow.com/a/8774101/1974918
+// run_return.js file not correctly loaded when running radiant design ui.R
 $(document).keydown(function(event) {
-  if ($("#doe_run").is(":visible") && (event.metaKey || event.ctrlKey) && event.keyCode == 13) {
-    $("#doe_run").click();
-  }
-});
-
-$(document).keydown(function(event) {
-  if ($("#doe_report").is(":visible") && event.altKey && event.keyCode == 13) {
-    $("#doe_report").click();
-  } else if ($("#sample_size_comp_report").is(":visible") && event.altKey && event.keyCode == 13) {
-    $("#sample_size_comp_report").click();
-  } else if ($("#sample_size_report").is(":visible") && event.altKey && event.keyCode == 13) {
-    $("#sample_size_report").click();
-  } else if ($("#sampling_report").is(":visible") && event.altKey && event.keyCode == 13) {
-    $("#sampling_report").click();
+  // ...uploads don't have a visibility property/method ...
+  if ($("#doe_download").is(":visible") && (event.metaKey || event.ctrlKey) && event.shiftKey === false && event.keyCode == 79) {
+    // file dialog pops up twice for some weird reason when using ui.R
+    // ... but it works when running radiant from package ...
+    // CMD-o to load factors
+    $("#doe_upload").click();
+    // document.getElementById("doe_upload").click();
+    event.preventDefault();
+  } else if ($("#doe_download").is(":visible") && (event.metaKey || event.ctrlKey) && event.shiftKey === false && event.keyCode == 83) {
+    // CMD-s to save factors
+    document.getElementById("doe_download").click();
+    event.preventDefault();
   }
 });
