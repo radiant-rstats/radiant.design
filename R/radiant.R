25486a64e7268672331a4c993b1e2a1bb3dcb8b2
vnijs <radiant@rady.ucsd.edu>
2018-11-20 16:38:33 -0800

    additional arguments for shiny::runApp

--- R/radiant.R
+++ R/radiant.R
@@ -3,6 +3,7 @@
 #' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
 #'
 #' @param state Path to state file to load
+#' @param ... additional arguments to pass to shiny::runApp (e.g, port = 8080)
 #'
 #' @importFrom radiant.data launch
 #'
@@ -11,13 +12,14 @@
 #' radiant.design()
 #' }
 #' @export
-radiant.design <- function(state) radiant.data::launch(package = "radiant.design", run = "browser", state)
+radiant.design <- function(state, ...) radiant.data::launch(package = "radiant.design", run = "browser", state, ...)
 
 #' Launch radiant.design in an Rstudio window
 #'
 #' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
 #'
 #' @param state Path to state file to load
+#' @param ... additional arguments to pass to shiny::runApp (e.g, port = 8080)
 #'
 #' @importFrom radiant.data launch
 #'
@@ -26,13 +28,14 @@ radiant.design <- function(state) radiant.data::launch(package = "radiant.design
 #' radiant.design_window()
 #' }
 #' @export
-radiant.design_window <- function(state) radiant.data::launch(package = "radiant.design", run = "window", state)
+radiant.design_window <- function(state, ...) radiant.data::launch(package = "radiant.design", run = "window", state, ...)
 
 #' Launch radiant.design in the Rstudio viewer
 #'
 #' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
 #'
 #' @param state Path to state file to load
+#' @param ... additional arguments to pass to shiny::runApp (e.g, port = 8080)
 #'
 #' @importFrom radiant.data launch
 #'
@@ -41,4 +44,4 @@ radiant.design_window <- function(state) radiant.data::launch(package = "radiant
 #' radiant.design_viewer()
 #' }
 #' @export
-radiant.design_viewer <- function(state) radiant.data::launch(package = "radiant.design", run = "viewer", state)
+radiant.design_viewer <- function(state, ...) radiant.data::launch(package = "radiant.design", run = "viewer", state, ...)
