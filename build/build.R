d4943b897017cd5a39dd201bd452c49d60a9e2d3
vnijs <vnijs@ucsd.edu>
2023-01-05 12:54:37 -0800

    build dropbox

--- build/build.R
+++ build/build.R
@@ -14,7 +14,10 @@ dirsrc <- "../minicran/src/contrib"
 if (rv < "3.4") {
   dirmac <- fs::path("../minicran/bin/macosx/mavericks/contrib", rv)
 } else if (rv > "3.6") {
-  dirmac <- fs::path("../minicran/bin/macosx/contrib", rv)
+  dirmac <- c(
+    fs::path("../minicran/bin/macosx/big-sur-arm64/contrib", rv),
+    fs::path("../minicran/bin/macosx/contrib", rv)
+  )
 } else {
   dirmac <- fs::path("../minicran/bin/macosx/el-capitan/contrib", rv)
 }
@@ -22,14 +25,18 @@ if (rv < "3.4") {
 dirwin <- fs::path("../minicran/bin/windows/contrib", rv)
 
 if (!fs::file_exists(dirsrc)) fs::dir_create(dirsrc, recursive = TRUE)
-if (!fs::file_exists(dirmac)) fs::dir_create(dirmac, recursive = TRUE)
+for (d in dirmac) {
+  if (!fs::file_exists(d)) fs::dir_create(d, recursive = TRUE)
+}
 if (!fs::file_exists(dirwin)) fs::dir_create(dirwin, recursive = TRUE)
 
-## delete older version of radiant
-rem_old <- function(app) {
-  unlink(paste0(dirsrc, "/", app, "*"))
-  unlink(paste0(dirmac, "/", app, "*"))
-  unlink(paste0(dirwin, "/", app, "*"))
+# delete older version of radiant
+rem_old <- function(pkg) {
+  unlink(paste0(dirsrc, "/", pkg, "*"))
+  for (d in dirmac) {
+    unlink(paste0(d, "/", pkg, "*"))
+  }
+  unlink(paste0(dirwin, "/", pkg, "*"))
 }
 
 sapply(pkg, rem_old)
@@ -40,19 +47,29 @@ system(paste0(Sys.which("R"), " -e \"setwd('", getwd(), "'); app <- '", pkg, "';
 win <- readline(prompt = "Did you build on Windows? y/n: ")
 if (grepl("[yY]", win)) {
 
+  fl <- list.files(pattern = "*.zip", path = "~/Dropbox/r-packages/", full.names = TRUE)
+  for (f in fl) {
+    print(f)
+    file.copy(f, "~/gh/")
+  }
+
   ## move packages to radiant_miniCRAN. must package in Windows first
   # path <- normalizePath("../")
   pth <- fs::path_abs("../")
 
   sapply(list.files(pth, pattern = "*.tar.gz", full.names = TRUE), file.copy, dirsrc)
   unlink("../*.tar.gz")
-  sapply(list.files(pth, pattern = "*.tgz", full.names = TRUE), file.copy, dirmac)
+  for (d in dirmac) {
+    sapply(list.files(pth, pattern = "*.tgz", full.names = TRUE), file.copy, d)
+  }
   unlink("../*.tgz")
   sapply(list.files(pth, pattern = "*.zip", full.names = TRUE), file.copy, dirwin)
   unlink("../*.zip")
 
-  tools::write_PACKAGES(dirmac, type = "mac.binary")
   tools::write_PACKAGES(dirwin, type = "win.binary")
+  for (d in dirmac) {
+    tools::write_PACKAGES(d, type = "mac.binary")
+  }
   tools::write_PACKAGES(dirsrc, type = "source")
 
   # commit to repo
@@ -64,3 +81,7 @@ if (grepl("[yY]", win)) {
 }
 
 setwd(curr)
+
+# remove.packages(c("radiant.model", "radiant.data"))
+# radiant.update::radiant.update()
+# install.packages("radiant.update")
